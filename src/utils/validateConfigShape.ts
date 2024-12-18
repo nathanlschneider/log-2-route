2e0add3e31b9aef66eae76ab69f86403627285b2
Nathan Schneider <nlschneider@gmail.com>
2024-12-18 01:35:53 -0500

    fixed checks for location and fileName in config can't be blank

--- src/utils/validateConfigShape.ts
+++ src/utils/validateConfigShape.ts
@@ -2,8 +2,12 @@ import { ConfigShape } from "../Types";
 import flattenJSON from "./flattenJSON";
 
 export default async function validateConfigShape(config: ConfigShape, loggerFileConfig: ConfigShape): Promise<boolean> {
+    if (!loggerFileConfig.logFile.location || !loggerFileConfig.logFile.fileName) {
+        throw new Error("Location and fileName cannot be blank strings");
+    }
+
     const configKeys = Object.keys(flattenJSON(config));
     const loggerConfigKeys = Object.keys(flattenJSON(loggerFileConfig));
   
     return configKeys.every(key => loggerConfigKeys.includes(key));
-  }
+}
