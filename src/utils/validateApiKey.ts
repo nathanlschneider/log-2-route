1222ebff17eb2b52aef8eafcc0af1506fe86b85d
Nathan Schneider <nlschneider@gmail.com>
2024-12-18 01:12:00 -0500

    Added additional check for api key

--- src/utils/validateApiKey.ts
+++ src/utils/validateApiKey.ts
@@ -1,4 +1,9 @@
 const validateApiKey = async (req: Request): Promise<boolean> => {
+
+  if (process.env.L2R_API_KEY === undefined || process.env.L2R_API_KEY === '') {
+    console.error('L2R api key is not set?');
+    return false
+  }
   const apiKey = req.headers.get('x-api-key');
   return apiKey === process.env.L2R_API_KEY;
 };
