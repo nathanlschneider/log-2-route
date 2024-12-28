3e56d15ed1bd2971a2b4b27119c9c45119571b39
Nathan Schneider <nlschneider@gmail.com>
2024-12-27 22:17:22 -0500

    added files to tsup bundle

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/config.js"],
+  entry: ["src/index.ts", "src/config.js", "install/route.ts", "install/l2r.config.json", "src/config/serverConfig.ts"],
   format: ["cjs", "esm"], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
   splitting: false,
