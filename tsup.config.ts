c059ca33b4c572e9e940c80213c457fd29d6b43b
Nathan Schneider <nlschneider@gmail.com>
2024-12-27 22:35:23 -0500

    changed bundle type in tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/config.js", "install/route.ts", "install/l2r.config.json", "src/config/serverConfig.ts"],
-  format: ["cjs", "esm"], // Build for commonJS and ESmodules
-  dts: true, // Generate declaration file (.d.ts)
+  entry: ["src/index.ts", "src/config.js"],
+  format: ["cjs", "esm"], 
+  dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
+  bundle: false,
 });
