2a7a973b548abf3928aa8bd7d0808822518cdd58
Nathan Schneider <nlschneider@gmail.com>
2024-12-28 22:27:49 -0500

    added missing export

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/utils", "src/types"],
-  format: ["cjs", "esm"], 
+  entry: ['src/index.ts', 'src/utils', 'src/types', 'src/config'],
+  format: ['cjs', 'esm'],
   dts: true,
   splitting: false,
   sourcemap: true,
