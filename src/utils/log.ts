22a3c3eb6f633186c9baa211d3bf642f2fbfee6b
nate-at-keen <nate@keen-studio.com>
2025-04-07 23:55:08 -0400

    changed logger fetch to /logger

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,9 +1,8 @@
 import type { BodyShape } from "../l2rTypes/l2rTypes";
 const log = async (data: BodyShape) => {
-  const location = typeof window !== "undefined" ? window.location.href : "http://localhost:3000/";
 
   try {
-    await fetch(`${location}logger`, {
+    await fetch(`/logger`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
