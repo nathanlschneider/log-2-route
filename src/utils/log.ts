a3b86646d15d45b94336586be2949d287eaf67be
nate-at-keen <nate@keen-studio.com>
2025-04-09 01:23:16 -0400

    more log changes

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,10 +1,14 @@
+
 import { getCurrentDomain } from "./getCurrentDomain";
 
 import type { BodyShape } from "../l2rTypes/l2rTypes";
 const log = async (data: BodyShape) => {
   const domain = await getCurrentDomain();
+  
+  const proto = process.env.NODE_ENV === "production" ? "https" : "http";
+
   try {
-    await fetch(`https://${domain}`, {
+    await fetch(`${proto}://${domain}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
