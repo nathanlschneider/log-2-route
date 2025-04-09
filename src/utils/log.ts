449c6790a11715efb74278b72fff92a2fc4d77ae
nate-at-keen <nate@keen-studio.com>
2025-04-09 10:32:03 -0400

    updated log

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,8 +1,6 @@
 import type { BodyShape } from "../l2rTypes/l2rTypes";
 const log = async (data: BodyShape) => {
 
-  // const proto = process.env.NODE_ENV === "production" ? "https" : "http";
-
   try {
     await fetch(`${window.location.origin}/logger`, {
       method: "POST",
