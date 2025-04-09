2f7448f8e2d074742ce964690392d9805b1fa49a
nate-at-keen <nate@keen-studio.com>
2025-04-09 08:37:22 -0400

    fixes

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,8 +1,10 @@
 import type { BodyShape } from "../l2rTypes/l2rTypes";
 const log = async (data: BodyShape) => {
 
+  // const proto = process.env.NODE_ENV === "production" ? "https" : "http";
+
   try {
-    await fetch(`/logger`, {
+    await fetch(`http://localhost:${window.location.port}/logger`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
